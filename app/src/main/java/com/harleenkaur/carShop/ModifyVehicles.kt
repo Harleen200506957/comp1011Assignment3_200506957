fb5e747d496b9ccb5895e2723621ace06228f76e
Harleen200506957 <200506957@student.georgianc.on.ca>
2023-04-17 11:12:11 +0530

    file added

---
+++ app/src/main/java/com/harleenkaur/carShop/ModifyVehicles.kt
@@ -0,0 +1,11 @@
+package com.harleenkaur.carShop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ModifyVehicles : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_modify_vehicles)
+    }
+}
---
+++ app/src/main/java/com/harleenkaur/carShop/soldvehicle.kt
@@ -0,0 +1,11 @@
+package com.harleenkaur.carShop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class soldvehicle : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_soldvehicle)
+    }
+}
